ce2f86dc6409aa585cb3890f07380b9077cddcc4
mitchell <mitchell.rian.smith@gmail.com>
2025-05-19 16:23:05 -0700

    add basic signin optins

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,22 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import { signIn, signOut, useSession } from "next-auth/react"
 
 export default function Home() {
+  const { data: session } = useSession()
+
+  if (session) {
+    return (
+      <>
+        <p>Signed in as {session.user?.name}</p>
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+
   return (
-    <div className={styles.page}>
-      <h1 className={styles.title}>Welcome to Next.js!</h1><h1></h1>
-    </div>
-  );
+    <>
+      <p>Not signed in</p>
+      <button onClick={() => signIn("azure-ad")}>Sign in with Microsoft</button>
+    </>
+  )
 }
